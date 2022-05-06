af7011ac1245ce4e78dc016c5882d848a707a43f
Victor Balogun <31784305+Kreitton@users.noreply.github.com>
2022-05-05 23:52:17 -0500

    Add files via upload

---
+++ BankSystem/AccountVisual.Designer.cs
@@ -0,0 +1,85 @@
+﻿
+namespace BankSystem
+{
+    partial class AccountVisual
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.depositBtn = new System.Windows.Forms.Button();
+            this.withdrawBtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(12, 146);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(224, 45);
+            this.richTextBox1.TabIndex = 4;
+            this.richTextBox1.Text = "";
+            // 
+            // depositBtn
+            // 
+            this.depositBtn.Location = new System.Drawing.Point(12, 237);
+            this.depositBtn.Name = "depositBtn";
+            this.depositBtn.Size = new System.Drawing.Size(109, 41);
+            this.depositBtn.TabIndex = 6;
+            this.depositBtn.Text = "Deposit";
+            this.depositBtn.UseVisualStyleBackColor = true;
+            this.depositBtn.Click += new System.EventHandler(this.depositBtn_Click);
+            // 
+            // withdrawBtn
+            // 
+            this.withdrawBtn.Location = new System.Drawing.Point(128, 237);
+            this.withdrawBtn.Name = "withdrawBtn";
+            this.withdrawBtn.Size = new System.Drawing.Size(109, 41);
+            this.withdrawBtn.TabIndex = 8;
+            this.withdrawBtn.Text = "Withdraw";
+            this.withdrawBtn.UseVisualStyleBackColor = true;
+            this.withdrawBtn.Click += new System.EventHandler(this.withdrawBtn_Click);
+            // 
+            // AccountVisual
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(696, 369);
+            this.Controls.Add(this.withdrawBtn);
+            this.Controls.Add(this.depositBtn);
+            this.Controls.Add(this.richTextBox1);
+            this.Name = "AccountVisual";
+            this.Text = "AccountVisual";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button depositBtn;
+        private System.Windows.Forms.Button withdrawBtn;
+    }
+}
---
+++ BankSystem/AccountVisual.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.VisualBasic;
+
+namespace BankSystem
+{
+    //BankSystem.RegistrationForm Acess;
+    public partial class AccountVisual : Form
+    {
+        public AccountVisual()
+        {
+            InitializeComponent();
+        }
+        private bool CheckInput(string x)
+        {
+            if (x.Length < 1) return false;
+            for (int i = 0; i < x.Length; ++i)
+            {
+                if (!(x[i] >= '0' && x[i] <= '9')) return false;
+            }
+            return true;
+        }
+      
+        private void depositBtn_Click(object sender, EventArgs e)
+        {
+            double Value;
+         
+            if (CheckInput(richTextBox1.Text))
+            {
+                
+                Value = Convert.ToDouble(richTextBox1.Text);
+                CustomerData.Current.AMT += Value;
+                
+                CustomerData.UpdateAcc();
+                MessageBox.Show("Your Order Has Been Processed!", "Confirm");
+                this.Close();
+  
+            }
+            else
+            {
+                MessageBox.Show("Please Enter A Correct Value!", "Error");
+            }
+
+        }
+
+        private void withdrawBtn_Click(object sender, EventArgs e)
+        {
+            if (CheckInput(richTextBox1.Text))
+            {
+                if (CustomerData.Current.AMT < Double.Parse(richTextBox1.Text))
+                {
+                    CustomerData.Current.AMT -= Double.Parse(richTextBox1.Text);
+                    MessageBox.Show("Withdrawal Successful!");
+
+                }
+                else
+                    MessageBox.Show("Insufficient Balance");
+            
+                this.Close();
+            }
+            else
+            {
+                MessageBox.Show("Invalid Entry");
+            }
+          
+        }
+    }
+}
